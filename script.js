ecb57c429de822aadf16dad494eb0aa9d8e75de9
ndonolli <ndonolli@gmail.com>
2024-10-01 09:45:31 -0500

    add expiry date

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello!');
+const now = new Date();
+
+let createDateString = date => {
+    const dateStr = date.toString()
+    const dateTokens = dateStr.split(' ');
+    const month = dateTokens[1];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    const ampm = hour >= 12 ? 'PM' : 'AM';
+    const formatted = `${month} ${day}, ${year}, ${hour}:${minute} ${ampm}`
+    console.log(formatted)
+    return formatted;
+}
+
+let expiryDateString = createDateString(now);
+const expiryDate = document.getElementById('expiry-date-string');
+expiryDate.innerText = expiryDateString;
