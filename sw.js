55f52771a5c0f378930e472753826f2e5b3f4ccf
ndonolli <ndonolli@gmail.com>
2024-10-08 07:47:55 -0500

    add offline caching

--- sw.js
+++ sw.js
@@ -1,3 +1,42 @@
-self.addEventListener('fetch', function (event) {
-    event.respondWith(fetch(event.request));
-  });
+const cacheName = "cache_v1.0.0";
+const precachedResources = [
+  "/",
+  "/index.html",
+  "/jpt_logo.webp",
+  "/rta_icon.png",
+  "/rta_logo.webp",
+  "/script.js",
+  "/styles.css"
+];
+
+async function precache() {
+  const cache = await caches.open(cacheName);
+  return cache.addAll(precachedResources);
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(precache());
+});
+
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open(cacheName);
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    return Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  if (precachedResources.includes(url.pathname)) {
+    event.respondWith(cacheFirst(event.request));
+  }
+});
