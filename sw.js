e3521bf8317e792fd5442de24eeb29b9d51f592e
ndonolli <ndonolli@gmail.com>
2024-10-08 07:56:20 -0500

    change caching strategy

--- sw.js
+++ sw.js
@@ -9,34 +9,13 @@ const precachedResources = [
   "/styles.css"
 ];
 
-async function precache() {
-  const cache = await caches.open(cacheName);
-  return cache.addAll(precachedResources);
-}
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(precache());
-});
-
-async function cacheFirst(request) {
-  const cachedResponse = await caches.match(request);
-  if (cachedResponse) {
-    return cachedResponse;
-  }
-  try {
-    const networkResponse = await fetch(request);
-    if (networkResponse.ok) {
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
       const cache = await caches.open(cacheName);
-      cache.put(request, networkResponse.clone());
-    }
-    return networkResponse;
-  } catch (error) {
-    return Response.error();
-  }
-}
-
-self.addEventListener("fetch", (event) => {
-  if (precachedResources.includes(url.pathname)) {
-    event.respondWith(cacheFirst(event.request));
-  }
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })(),
+  );
 });
