d1b1f4ce9bffaacf21fe59f8dc2106d01b90fa7e
null <null>
2026-09-18 19:02:56 -0500

    delete old caches, update cache version again

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const cacheName = "cache_v1.0.4";
+const cacheName = "cache_v1.1.0";
+
 const contentToCache = [
   "/",
   "/index.html",
@@ -11,28 +12,47 @@ const contentToCache = [
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install");
+
   e.waitUntil(
-    (async () => {
-      const cache = await caches.open(cacheName);
-      console.log("[Service Worker] Caching all: app shell and content");
-      await cache.addAll(contentToCache);
-    })(),
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  console.log("[Service Worker] Activate");
+
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== cacheName)
+          .map((name) => caches.delete(name))
+      );
+    })
   );
 });
 
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
-      const r = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (r) {
-        return r;
-      }
-      const response = await fetch(e.request);
       const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
+
+      // Only check the CURRENT cache
+      const cachedResponse = await cache.match(e.request);
+
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      const response = await fetch(e.request);
+
+      if (response.ok) {
+        await cache.put(e.request, response.clone());
+      }
+
       return response;
-    })(),
+    })()
   );
 });
